75061f429c02a404b5d988fd56feb4604cb0a281
Ricardo Campos <ricardocamposgarcia@gmail.com>
2019-03-14 12:05:05 +0100

    initial commit

---
+++ src/app/content/content.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-content',
+  templateUrl: './content.component.html',
+  styleUrls: ['./content.component.css']
+})
+export class ContentComponent {
+
+  people = [
+    {'name': 'Jose', 'age': '23', 'country': 'ES'},
+    {'name': 'Sara', 'age': '30', 'country': 'FR'},
+    {'name': 'Tomás', 'age': '31', 'country': 'ES'},
+    {'name': 'Carla', 'age': '26', 'country': 'FR'},
+    {'name': 'Laura', 'age': '29', 'country': 'UK'},
+  ];
+}
